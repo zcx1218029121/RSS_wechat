e76d0bb5e14dd83ad925fa5ebb8e64d0cfdfe723
xct352131424 <xct352131424@163.com>
2019-08-12 16:51:54 +0800

    pip 实现2级缓存

--- src/pipe/Pipe.py
+++ src/pipe/Pipe.py
@@ -2,6 +2,7 @@ import time
 
 
 # 数据格式化
+# 数据缓存
 def format_context(items):
     context = ""
     try:
@@ -17,33 +18,29 @@ class Pipe:
     # 回复数据的组装
     # 抓取数据的持久化
     # 抓取内容的缓存 当前只实现了二级缓存
-    def __init__(self, providers, valid_time=0):
+    def __init__(self, valid_time=0):
         # 缓存的有效时间 单位秒
         self.valid_time = valid_time
         # 内存的缓存字典
         self._cache = {}
-        # 数据提供类
-        self._provider = providers
 
-    # 获取数据缓存
-    # date
-    def get_date(self):
-        url = self._provider.url
+
+
+    def get_date(self,provider):
+        url = provider.url
+        # 检查缓存是否存在
         if self._cache.__contains__(url):
-            # 检查缓存是否存在
             temp_cache_item = self._cache[url]
+            # 检查缓存是否超时
             if temp_cache_item['un_valid_time'] > time.time():
                 print("从缓存中获取")
                 return temp_cache_item['context']
-            else:
-                return self.update_cache(url)
         else:
-            return self.update_cache(url)
+            return self.update_cache(provider)
 
-    def update_cache(self, url):
+    def update_cache(self, provider):
         print("内存缓存被击穿")
-        contexts = format_context(self._provider.score)
-
+        contexts = format_context(provider.score)
         cache_item = {'context': contexts, 'un_valid_time': time.time() + self.valid_time}
-        self._cache[url] = cache_item
+        self._cache[provider.url] = cache_item
         return contexts
