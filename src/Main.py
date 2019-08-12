65fc8fc726b2e4c2fd2f427bcdb9eb5cf985a2ac
xct352131424 <xct352131424@163.com>
2019-08-12 16:50:11 +0800

    pip 实现2级缓存

--- src/Main.py
+++ src/Main.py
@@ -7,10 +7,13 @@ from pipe.Pipe import Pipe
 import time
 
 __author__ = "短狐"
-cache = {}  # cache 字典对象
+
 bot = Bot(cache_path=True)
 map = {}  # 消息源
 fd = []  # 订阅者
+providers = {}  # 数据提供者字典 用的是懒加载用到的时候再加载避免浪费RAM
+cacheTime = 0
+pip = Pipe(valid_time=60 * 60 * 2)
 
 
 def init_config():
@@ -18,53 +21,68 @@ def init_config():
     job = json.loads(f.read())
     data = job['source']
     sb = job['subscriber']
+    cache_time = job['cacheTime']
 
-    # initsource
     f.close()
+    read_source(data)
+    # read_subscriber(sb,bot)
+    init_pip(cache_time)
+
+
+# clear html
+def clear_content(context):
+    return re.sub(r"</?(.+?)>", "", context)
+
+
+# 读取订阅源
+def read_source(data):
     for temp in data:
         map[temp['msg']] = temp
 
-    # init   friend
+
+# 读取订阅者
+# Param: sb 订阅者数组
+def read_subscriber(sb):
     for f in sb:
         fd.append(bot.friends().search(f['name'])[0])
 
 
-def delAllHtml(context):
-    return re.sub(r"</?(.+?)>", "", context)
+# 根据用户询问获取对应的url
+def get_reply_url(msg):
+    return map[msg]['url']
 
 
-def doReply(msg, cd=delAllHtml):
-    s = ""
-    try:
-        p = Provider(map[msg]
-                     ['url'], item=map[msg]['size'], cd=cd).score
+# 初始化数据提供者
+def init_Provider(msg):
+    return Provider(get_reply_url(msg), item=map[msg]['size'], cd=clear_content)
 
-        for j in p:
-            if map[msg]['des'] == 0:
-                s += j.title + j.link + "\n"
-            else:
-                s += j.title + j.context + j.link + "\n"
 
-        return s
-
-    except KeyError:
-        KeyError.args
-        return
+# parm:msg 用户回复
+# return：返回数据提供者
+# 获取数据提供者并初始化
+def get_Provider(msg):
+    if providers.__contains__(msg):
+        return providers[msg]
+    else:
+        print("初始化数据提供者 ing .....")
+        providers[msg] = init_Provider(msg)
+        return providers[msg]
 
 
 @bot.register(fd)
 def reply_my_friend(msg):
     if msg.text in map:
-        return '{} '.format(doReply(msg.text))
+        print(msg)
+        print(pip.get_date(get_Provider(msg)))
+        return '{} '.format(pip.get_date(get_Provider(msg)))
     else:
         return
 
 
+def init_pip(cache_time):
+    pip = Pipe(cache_time)
+
+
 if __name__ == "__main__":
-    # init_config()
-    pip = Pipe(providers=Provider('https://rsshub.app/xueqiu/user/8152922548', item=5), valid_time=1)
-    pip.get_date()
-    pip.get_date()
-    pip.get_date()
-    time.sleep(20)
-    pip.get_date()
+    init_config()
+    embed()
